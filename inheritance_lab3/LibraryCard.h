11e51837c62bebf3426785aebc28e4e8f526168f
Qr1te <roman.dashkevich3@gmail.com>
2025-09-23 09:18:36 +0300

    qr

--- inheritance_lab3/LibraryCard.h
+++ inheritance_lab3/LibraryCard.h
@@ -4,36 +4,34 @@
 #include <string>
 #include "Article.h"
 
-using namespace std;
-
 class LibraryCard {
 protected:
-    string author;
-    string title;
-    string authorSign;
-    string inventoryNumber;
-    string thematicCode;
+    std::string author;
+    std::string title;
+    std::string authorSign;
+    std::string inventoryNumber;
+    std::string thematicCode;
 public:
-    LibraryCard(const string& a, const string& t, const string& as, const string& in, const string& tc);
-    string getAuthor() const;
-    string getTitle() const;
-    string getAuthorSign() const;
-    string getInventoryNumber() const;
-    string getThematicCode() const;
-    string getAlphabeticalSearch() const;
+    LibraryCard(const std::string& a, const std::string& t, const std::string& as, const std::string& in, const std::string& tc);
+    std::string getAuthor() const;
+    std::string getTitle() const;
+    std::string getAuthorSign() const;
+    std::string getInventoryNumber() const;
+    std::string getThematicCode() const;
+    std::string getAlphabeticalSearch() const;
     virtual ~LibraryCard() ;
 };
 
 class IndependentPublicationCard : public LibraryCard {
 protected:
-    string publisher;
+    std::string publisher;
     int year;
     int circulation;
     int pages;
 public:
-    IndependentPublicationCard(const string& a, const string& t, const string& as, const string& in, const string& tc,
-                               const string& p, int y, int c, int pg);
-    string getPublisher() const;
+    IndependentPublicationCard(const std::string& a, const std::string& t, const std::string& as, const std::string& in, const std::string& tc,
+                               const std::string& p, int y, int c, int pg);
+    std::string getPublisher() const;
     int getYear() const;
     int getCirculation() const;
     int getPages() const;
@@ -44,7 +42,7 @@ private:
     Article article_;
     IndependentPublicationCard* publication_;
 public:
-    ArticleCard(const string& as, const string& in, const string& tc,
+    ArticleCard(const std::string& as, const std::string& in, const std::string& tc,
                 const Article& art, IndependentPublicationCard* pub);
     const Article& getArticle() const;
     IndependentPublicationCard* getPublication() const;
@@ -56,10 +54,10 @@ private:
     ArticleCard* articleCards_[MAX_ARTICLES];
     int articleCount_;
 public:
-    ArticleCollectionCard(const string& a, const string& t, const string& as, const string& in, const string& tc,
-                          const string& p, int y, int c, int pg);
+    ArticleCollectionCard(const std::string& a, const std::string& t, const std::string& as, const std::string& in, const std::string& tc,
+                          const std::string& p, int y, int c, int pg);
     ~ArticleCollectionCard() final;
-    void addArticle(const Article& art, const string& as, const string& in, const string& tc);
+    void addArticle(const Article& art, const std::string& as, const std::string& in, const std::string& tc);
     int getArticleCount() const;
     const ArticleCard* getArticleCard(int index) const;
 };
@@ -68,8 +66,8 @@ class BookCard : public IndependentPublicationCard {
 private:
     Article article_;
 public:
-    BookCard(const string& as, const string& in, const string& tc,
-             const string& p, int y, int c, int pg, const Article& art);
+    BookCard(const std::string& as, const std::string& in, const std::string& tc,
+             const std::string& p, int y, int c, int pg, const Article& art);
     const Article& getArticle() const;
 };
 
