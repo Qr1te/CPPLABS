3731941d6e719963638ac0ef03cafd4820c6a1bf
Qr1te <roman.dashkevich3@gmail.com>
2025-10-01 22:01:28 +0300

    qr

--- lab3/includes/ArticleCollectionCard.h
+++ lab3/includes/ArticleCollectionCard.h
@@ -1,17 +1,18 @@
 #ifndef LAB3_ARTICLECOLLECTIONCARD_H
 #define LAB3_ARTICLECOLLECTIONCARD_H
 
-#include "IndependentPublicationCard.h"
 #include "ArticleCard.h"
-#include <string>
+#include "IndependentPublicationCard.h"
+#include <vector>
 #include <cstring>
+#include <memory>
 
 
 class ArticleCollectionCard : public IndependentPublicationCard {
 private:
+private:
     static const int MAX_ARTICLES = 100;
-    ArticleCard* articleCards[MAX_ARTICLES];
-    int articleCount = 0;
+    std::vector<std::unique_ptr<ArticleCard>> articleCards;
 public:
     ArticleCollectionCard(const std::string& a, const std::string& t, const std::string& as, const std::string& in, const std::string& tc);
     ArticleCollectionCard(const ArticleCollectionCard& other);
--- lab3/src/ArticleCollectionCard.cpp
+++ lab3/src/ArticleCollectionCard.cpp
@@ -1,49 +1,44 @@
 #include "../includes/ArticleCollectionCard.h"
+#include <memory>
 
 using namespace std;
 
 ArticleCollectionCard::ArticleCollectionCard(const string& a, const string& t, const string& as, const string& in, const string& tc)
-    : IndependentPublicationCard(a, t, as, in, tc){
-    memset(articleCards, 0, sizeof(articleCards));
+    : IndependentPublicationCard(a, t, as, in, tc) {
 }
 
-ArticleCollectionCard::ArticleCollectionCard(const ArticleCollectionCard &other): IndependentPublicationCard(other){
-    for (int i = 0; i < articleCount; ++i) {
-        articleCards[i] = new ArticleCard(*other.articleCards[i]);
+ArticleCollectionCard::ArticleCollectionCard(const ArticleCollectionCard &other): IndependentPublicationCard(other) {
+    for (const auto& card : other.articleCards) {
+        articleCards.push_back(make_unique<ArticleCard>(*card));
     }
 }
 
-ArticleCollectionCard &ArticleCollectionCard:: operator=(const ArticleCollectionCard& other) {
+ArticleCollectionCard &ArticleCollectionCard::operator=(const ArticleCollectionCard& other) {
     if (this != &other) {
         IndependentPublicationCard::operator=(other);
-        for (int i = 0; i < articleCount; ++i) {
-            delete articleCards[i];
-        }
-        articleCount = other.articleCount;
-        for (int i = 0; i < articleCount; ++i) {
-            articleCards[i] = new ArticleCard(*other.articleCards[i]);
+        articleCards.clear();
+        for (const auto& card : other.articleCards) {
+            articleCards.push_back(make_unique<ArticleCard>(*card));
         }
     }
     return *this;
 }
 
-ArticleCollectionCard::~ArticleCollectionCard() {
-    for (int i = 0; i < articleCount; ++i) {
-        delete articleCards[i];
-    }
-}
+ArticleCollectionCard::~ArticleCollectionCard() = default;  // деструктор не нужен!
 
 void ArticleCollectionCard::addArticle(const Article& art, const string& as, const string& in, const string& tc) {
-    if (articleCount < MAX_ARTICLES) {
-        articleCards[articleCount++] = new ArticleCard(as, in, tc, art, this);
+    if (articleCards.size() < MAX_ARTICLES) {
+        articleCards.push_back(make_unique<ArticleCard>(as, in, tc, art, this));
     }
 }
 
-int ArticleCollectionCard::getArticleCount() const { return articleCount; }
+int ArticleCollectionCard::getArticleCount() const {
+    return articleCards.size();
+}
 
 const ArticleCard* ArticleCollectionCard::getArticleCard(int index) const {
-    if (index >= 0 && index < articleCount) {
-        return articleCards[index];
+    if (index >= 0 && index < articleCards.size()) {
+        return articleCards[index].get();
     }
     return nullptr;
 }
