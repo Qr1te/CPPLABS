5a580cd9ed65c100a8ef32d63520a70ae7ff31c2
Qr1te <roman.dashkevich3@gmail.com>
2025-10-01 21:39:22 +0300

    qr

--- lab3/src/AlphabeticalCatalog.cpp
+++ lab3/src/AlphabeticalCatalog.cpp
@@ -1,11 +1,14 @@
 #include "../includes/AlphabeticalCatalog.h"
 
-int AlphabeticalCatalog::search(const std::string_view & query, LibraryCard** results, int maxResults) const {
+int AlphabeticalCatalog::search(const std::string_view& query, LibraryCard** results, int maxResults) const {
     int count = 0;
-    for (int i = 0; i < getCardCount() && count < maxResults; ++i) {
+    const int totalCards = getCardCount();
+
+    for (int i = 0; i < totalCards && count < maxResults; ++i) {
         LibraryCard* card = getCard(i);
         if (card != nullptr && card->getAlphabeticalSearch() == query) {
-            results[count++] = card;
+            results[count] = card;
+            ++count;
         }
     }
     return count;
--- lab3/src/ThematicCatalog.cpp
+++ lab3/src/ThematicCatalog.cpp
@@ -1,11 +1,16 @@
 #include "../includes/ThematicCatalog.h"
 
-int ThematicCatalog::search(const std::string_view & code, LibraryCard** results, int maxResults) const {
+int ThematicCatalog::search(const std::string_view& code, LibraryCard** results, int maxResults) const {
     int count = 0;
-    for (int i = 0; i < getCardCount() && count < maxResults; ++i) {
+    const int totalCards = getCardCount();
+
+    for (int i = 0; i < totalCards; ++i) {
+        if (count >= maxResults) break;
+
         LibraryCard* card = getCard(i);
-        if (card != nullptr && card->getThematicCode() == code) {
-            results[count++] = card;
+        if (card && card->getThematicCode() == code) {
+            results[count] = card;
+            ++count;
         }
     }
     return count;
