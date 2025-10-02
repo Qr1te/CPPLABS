0113440964a9e55df60811dad19c6d239c240a93
Qr1te <roman.dashkevich3@gmail.com>
2025-10-02 12:22:34 +0300

    qr

--- lab4/src/functions.cpp
+++ lab4/src/functions.cpp
@@ -118,15 +118,20 @@ void menu() {
             delete shape;
             shape = nullptr;
         }
-        bool shouldBreak = false;
-        if (continueChoice == 1) {
-            shouldBreak = true;
-        } else if (continueChoice == 2) {
-            continueProgram = false;
-            shouldBreak = true;
-        } else {
-            std::cout << "Invalid choice! Please enter 1 or 2.\n";
-        }
+
+        while (true) {
+            displayContinueMenu();
+            std::cin >> continueChoice;
+
+            if (continueChoice == 1) {
+                break;
+            } else if (continueChoice == 2) {
+                continueProgram = false;
+                break;
+            } else {
+                std::cout << "Invalid choice! Please enter 1 or 2.\n";
+            }
+    }
     delete shape;
 
     std::cout << "\nThank you for using Geometric Figures Program!\n";
