584ea4581168e7c69909ccd7d2b0e5716a08ac41
Qr1te <roman.dashkevich3@gmail.com>
2025-10-02 12:14:20 +0300

    qr

--- lab4/src/functions.cpp
+++ lab4/src/functions.cpp
@@ -1,76 +1,140 @@
 #include "../includes/functions.h"
 
-void displayMenu() {
-    std::cout << "\nMenu:\n";
-    std::cout << "1. Add Circle\n";
-    std::cout << "2. Add Square\n";
-    std::cout << "3. Add Sphere\n";
-    std::cout << "4. Add Cube\n";
-    std::cout << "5. Exit\n";
-    std::cout << "Enter choice: ";
+
+template <typename T> T getInput(const char *msg){
+    T value;
+    std::cout << msg;
+
+    while (true)
+    {
+        if (std::cin >> value)
+        {
+            if (std::cin.get() == '\n')
+            {
+                return value;
+            }
+            std::cin.clear();
+            while (std::cin.get() != '\n' && !std::cin.eof())
+                ;
+            std::cout << "Error, invalid input. Please try again: ";
+        }
+        else
+        {
+            std::cin.clear();
+            while (std::cin.get() != '\n' && !std::cin.eof())
+                ;
+            std::cout << "Error, invalid input. Please try again: ";
+        }
+    }
 }
 
-void menu(){
-    Shape* shapes = nullptr;
+void displayMenu() {
+    std::cout << "\n=== Geometric Figures Menu ===\n";
+    std::cout << "1. Create Circle\n";
+    std::cout << "2. Create Square\n";
+    std::cout << "3. Create Sphere\n";
+    std::cout << "4. Create Cube\n";
+    std::cout << "5. Exit Program\n";
+    std::cout << "Enter your choice: ";
+}
+
+void displayContinueMenu() {
+    std::cout << "\n=== What would you like to do? ===\n";
+    std::cout << "1. Continue - create another figure\n";
+    std::cout << "2. Exit Program\n";
+    std::cout << "Enter your choice: ";
+}
+
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void menu() {
+    Shape* shape = nullptr;
     int choice;
+    int continueChoice;
     double param1;
     double figureArea;
     double figureVolume;
+    bool continueProgram = true;
 
-    while (true) {
+    while (continueProgram) {
+        clearScreen();
         displayMenu();
         std::cin >> choice;
-        if (choice == 5) break;
-        delete shapes;
-        shapes = nullptr;
+
+        if (choice == 5) {
+            break;
+        }
         switch (choice) {
             case 1:
                 std::cout << "Enter radius: ";
-                std::cin >> param1;
-                shapes = new Circle(param1);
-                shapes->print();
-                figureArea = shapes->area();
-                std::cout << "Area: " << figureArea << std::endl;
-                shapes->volume();
-                shapes->draw();
+                param1 = getInput<double>("");
+                shape = new Circle(param1);
                 break;
             case 2:
                 std::cout << "Enter side: ";
-                std::cin >> param1;
-                shapes = new Square(param1);
-                shapes->print();
-                figureArea = shapes->area();
-                std::cout << "Area: " << figureArea << std::endl;
-                shapes->volume();
-                shapes->draw();
+                param1 = getInput<double>("");
+                shape = new Square(param1);
                 break;
             case 3:
                 std::cout << "Enter radius: ";
-                std::cin >> param1;
-                shapes = new Sphere(param1);
-                shapes->print();
-                figureArea = shapes->area();
-                std::cout << "Area: " << figureArea << std::endl;
-                figureVolume = shapes->volume();
-                std::cout << "Volume: " << figureVolume << std::endl;
-                shapes->draw();
+                param1 = getInput<double>("");
+                shape = new Sphere(param1);
                 break;
             case 4:
                 std::cout << "Enter side: ";
-                std::cin >> param1;
-                shapes = new Cube(param1);
-                shapes->print();
-                figureArea = shapes->area();
-                std::cout << "Area: " << figureArea << std::endl;
-                figureVolume = shapes->volume();
-                std::cout << "Volume: " << figureVolume << std::endl;
-                shapes->draw();
+                param1 = getInput<double>("");
+                shape = new Cube(param1);
                 break;
             default:
-                std::cout << "Invalid choice!\n";
+                std::cout << "Invalid choice! Please try again.\n";
+                continue;
+        }
+
+        if (shape != nullptr) {
+            clearScreen();
+            std::cout << "=== Created Figure Information ===\n";
+            shape->print();
+            figureArea = shape->area();
+            std::cout << "Area: " << figureArea << std::endl;
+
+
+            if (choice == 3 || choice == 4) {
+                figureVolume = shape->volume();
+                std::cout << "Volume: " << figureVolume << std::endl;
+            } else {
+                shape->volume();
+            }
+
+            std::cout << "\nVisualization:\n";
+            shape->draw();
+
+
+            delete shape;
+            shape = nullptr;
+        }
+
+        while (true) {
+            displayContinueMenu();
+            std::cin >> continueChoice;
+
+            if (continueChoice == 1) {
+                break;
+            } else if (continueChoice == 2) {
+
+                continueProgram = false;
+                break;
+            } else {
+                std::cout << "Invalid choice! Please enter 1 or 2.\n";
+            }
         }
     }
-    delete shapes;
+    delete shape;
+
+    std::cout << "\nThank you for using Geometric Figures Program!\n";
 }
-
-
